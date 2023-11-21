e0137474b640ef14ed9b4dd39aa695f0c22a13ba
Vinicius Peruzza <vinicius@peruzza.com.br>
2023-11-21 07:19:18 -0300

    QoL changes

--- src/components/TextModal/index.jsx
+++ src/components/TextModal/index.jsx
@@ -6,16 +6,24 @@ export const TextModal = ({ isOpen, handleSubmit }) => {
   if (!isOpen) {
     return <></>;
   }
+
+  const onSubmit = (content) => content && handleSubmit(content);
+
+  const handlePressEnter = (event) =>
+    event.code === "Enter" && onSubmit(textContent);
+
   return (
     <div className="modal__container">
       <input
+        autoFocus
         className="modal__input"
         onChange={(event) => setTextContent(event.target.value)}
+        onKeyDown={(event) => handlePressEnter(event)}
       />
       <button
         className="modal__button"
         onClick={() => {
-          handleSubmit(textContent);
+          onSubmit(textContent);
         }}
       >
         Adicionar
